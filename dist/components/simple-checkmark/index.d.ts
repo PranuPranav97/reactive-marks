51638a7da036f679d555c75baa9d116735373d33
Pranav <pranavshastri07@gmail.com>
2023-08-03 17:29:48 +0530

    fix: dist removed from src and Types Added.

--- dist/components/simple-checkmark/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { ISimpleCheckmarkProps } from "../../types/simple-checkmark";
-export declare const SimpleCheckMark: React.FC<ISimpleCheckmarkProps>;
-//# sourceMappingURL=index.d.ts.map
--- dist/types/common/index.d.ts
+++
@@ -1,2 +0,0 @@
-export type BackgroundColor = "transparent" | "inherit" | "initial" | "currentColor" | string;
-//# sourceMappingURL=index.d.ts.map

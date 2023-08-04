5d8066a99f819cc201078865bbe8cba7fabf82c2
Pranav <pranavshastri07@gmail.com>
2023-08-04 18:27:20 +0530

    fix: success checkmark export issue.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export * from "./components/checkmark-classic";
 export * from "./components/simple-checkmark";
 export * from "./components/circled-checkmark";
+export * from "./components/success-checkmark";
